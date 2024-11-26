3c03c2136e4efb0b2b5fd2c2f880b9e6bd87f923
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-26 18:39:18 +0530

    Create replace all occurrences of a substring with another substring string.py

---
+++ coding-assignment/string/replace all occurrences of a substring with another substring string.py
@@ -0,0 +1,3 @@
+'''Write a Python program to replace all occurrences of a substring with another substring.'''
+def replace_substring(input_string, old_substring, new_substring):
+ return input_string.replace(old_substring, new_substring)
