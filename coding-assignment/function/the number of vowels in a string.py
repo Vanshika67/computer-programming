62710d802a97e0470a31d56ebdacf62091a13275
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-26 20:32:47 +0530

    Create the number of vowels in a string.py

---
+++ coding-assignment/function/the number of vowels in a string.py
@@ -0,0 +1,5 @@
+'''Write a Python function to count the number of vowels in a string.'''
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in s if char in vowels)
+print(count_vowels("hello world")) 
