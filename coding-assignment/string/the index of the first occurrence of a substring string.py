23a251d375c94332717b360e5443362003717637
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-26 18:36:17 +0530

    Create the index of the first occurrence of a substring string.py

---
+++ coding-assignment/string/the index of the first occurrence of a substring string.py
@@ -0,0 +1,3 @@
+'''Write a Python program to find the index of the first occurrence of a substring in a string.''' 
+def find_substring(input_string, substring):
+ return input_string.find(substring) 
