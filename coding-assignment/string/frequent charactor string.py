7b005c2f0a3498f1b0a5103a12283db543951dca
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-26 18:13:55 +0530

    Create frequent charactor string.py

---
+++ coding-assignment/string/frequent charactor string.py
@@ -0,0 +1,6 @@
+'''Write a Python program to find the most frequent character in a string.'''
+def most_frequent_char(input_string):
+ char_count = {}
+ for char in input_string:
+     char_count[char] = char_count.get(char, 0) + 1
+ return max(char_count, key=char_count.get)  
