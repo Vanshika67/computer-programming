22458c9f832d0852c7150f85799ed95abf0b3968
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-27 14:29:22 +0530

    Create an element in a tuple.py

---
+++ coding-assignment/tuple/an element in a tuple.py
@@ -0,0 +1,10 @@
+'''Access an element in a tuple.'''
+
+# Solution 1:
+def access_element(tup, index):
+    return tup[index]
+
+# Solution 2:
+tup = (10, 20, 30, 40, 50)
+element = tup[2]
+print(element) 
