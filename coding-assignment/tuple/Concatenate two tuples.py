44885e0c93d2660b7dc860d3225801c338c51c4c
Vanshika67 <vanshikamohan06@gmail.com>
2024-11-27 14:28:18 +0530

    Create Concatenate two tuples.py

---
+++ coding-assignment/tuple/Concatenate two tuples.py
@@ -0,0 +1,11 @@
+'''Concatenate two tuples.'''
+
+# Solution 1:
+def concatenate_tuples(tup1, tup2):
+    return tup1 + tup2
+
+# Solution 2:
+tup1 = (1, 2, 3)
+tup2 = (4, 5, 6)
+result = tup1 + tup2
+print(result) 
